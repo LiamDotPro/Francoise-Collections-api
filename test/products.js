056d4b519effc5f5aa9aabdcbde8f30d72a1cb9c
Liam Read <liam.read@students.plymouth.ac.uk>
2018-04-11 23:01:33 +0100

    extra product test for invetory

--- test/products.js
+++ test/products.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill';
 import ProductBase from '../library/Products/productsBase';
+import InventoriesBase from '../library/Inventories/InventoriesBase';
 
 let assert = require('chai').assert;
 
 const products = new ProductBase();
+const inventories = new InventoriesBase();
 
 describe('Products', () => {
     describe('Products Base Library', () => {
@@ -14,6 +16,11 @@ describe('Products', () => {
             let result = await products.createProduct('Test Product', 'This is a test item', 'thumbnail.png', 'Dispatches in 2-4 days', 0, false, new Date("2018-03-31 02:00:07.525+01"), new Date("2018-03-31 02:00:07.525+01"));
             createdItemId = result.insertedId;
             return assert.equal(0, result.payload, result.msg);
+        });
+
+        it('Should find an inventory for the product', async () => {
+            let result = await inventories.getInventoryByProductId(createdItemId);
+            assert.equal(result.inventory.productIdentifer, createdItemId, result.msg);
         });
 
         it('Should update the previously made product', async () => {
